78c7efde0292ab22a9ffbbf3129d6eb9fa85b4d1
Mark Robitaille <mark.rj.robitaille@gmail.com>
2021-01-19 22:45:14 -0600

    Updated description on the main page

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -8,9 +8,9 @@
     <b-row class="page-content">
       <b-col>
         <h3 id="hithere">Hi There!</h3>
-        <p>My name is Mark Robitaille. I am a computer science major at the University of Manitoba. My current passions are web and video game development. I am passionate about learning new technologies and want to learn as much as possible.</p>
+        <p>My name is Mark Robitaille, and I am a fourth-year computer science student at the University of Manitoba. I am very interested in both web development and video game development.</p>
         <p class="mb-0">
-          One of the things I value most is being a part of a community. I act as one of the executives in the Computer Science Students' Association at the University of Manitoba, where I help coordinate events for students to enjoy like game jams and social events. On top of that, I help maintain and promote our lounge to encourage new computer science students to mingle with and learn from more experienced students.
+          One of the things I value most is being a part of a community. For nearly two years, I have acted as vice president of the University of Manitoba Computer Science Students’ Association (CSSA). This has given me a chance to meet many incredible people and coordinate events for students to enjoy, like game jams and social events. On top of that, I help maintain and promote our lounge and community to encourage new computer science students to mingle with and learn from more experienced students.
         </p>
       </b-col>
     </b-row>
